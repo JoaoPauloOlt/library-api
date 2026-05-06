153a5bb8e40757003b088467b8eeb6534f4a4a15
João Paulo <jpoltramari@gmail.com>
2026-05-06 11:44:00 -0300

    feature: Adicionando Grupos e permissões

---
+++ src/main/java/com/jpoltramari/library_api/domain/model/Group.java
@@ -0,0 +1,36 @@
+package com.jpoltramari.library_api.domain.model;
+
+import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "groups")
+@EqualsAndHashCode
+public class Group {
+
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Short id;
+
+    @Column(nullable = false, unique = true, length = 50)
+    private String name;
+
+    @Column(length = 50)
+    private String description;
+
+    @ManyToMany
+    @JoinTable(
+            name = "group_permissions",
+            joinColumns = @JoinColumn(name = "group_id"),
+            inverseJoinColumns = @JoinColumn(name = "permission_id")
+    )
+    private List<Permissions> permissions = new ArrayList<>();
+}
