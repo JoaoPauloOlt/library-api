7723b2b9b52914db8c33d327c78ac34a8a3d7ce7
João Paulo <jpoltramari@gmail.com>
2026-05-06 13:07:15 -0300

    feat: adiciona queries para RBAC e regras de negocios para emprestimo

--- src/main/java/com/jpoltramari/library_api/domain/repository/LoanRepository.java
+++ src/main/java/com/jpoltramari/library_api/domain/repository/LoanRepository.java
@@ -1,6 +1,7 @@
 package com.jpoltramari.library_api.domain.repository;
 
 import com.jpoltramari.library_api.domain.model.Loan;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
@@ -9,4 +10,14 @@ public interface LoanRepository extends CustomJpaRepository<Loan, Long>{
     List<Loan> findByUserId(Long userId);
 
     List<Loan> findByBookId(Long bookId);
+
+    List<Loan> findByStatus(String status);
+
+    @Query("""
+        SELECT COUNT(1)
+        FROM Loan l
+        WHERE l.user.id = :userId
+        AND l.status IN ('SOLICITADO', 'APROVADO', 'RETIRADO', 'ATRASADO')
+""")
+    long countActiveLoansByUser(Long userId);
 }
--- src/main/java/com/jpoltramari/library_api/domain/repository/UserRepository.java
+++ src/main/java/com/jpoltramari/library_api/domain/repository/UserRepository.java
@@ -1,11 +1,21 @@
 package com.jpoltramari.library_api.domain.repository;
 
 import com.jpoltramari.library_api.domain.model.User;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
 public interface UserRepository extends CustomJpaRepository<User, Long>{
 
     Optional<User> findByEmail(String email);
+
     boolean existsByEmail(String email);
+
+    @Query("""
+        SELECT u FROM User u
+        LEFT JOIN FETCH u.groups g
+        LEFT JOIN FETCH g.permissions
+        WHERE u.email = :email
+""")
+    Optional<User> findByEmailWithGroupsAndPermissions(String email);
 }
