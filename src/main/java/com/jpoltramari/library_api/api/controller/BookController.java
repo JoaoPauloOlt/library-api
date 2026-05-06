a5907374e8a0b68214e10532e919c96a099dc539
João Paulo <jpoltramari@gmail.com>
2026-05-06 13:06:17 -0300

    feat: refatora controllers para suportar RBAC e novo flow de emprestimo

--- src/main/java/com/jpoltramari/library_api/api/controller/BookController.java
+++ src/main/java/com/jpoltramari/library_api/api/controller/BookController.java
@@ -8,7 +8,6 @@ import com.jpoltramari.library_api.domain.model.Book;
 import com.jpoltramari.library_api.domain.service.BookService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
