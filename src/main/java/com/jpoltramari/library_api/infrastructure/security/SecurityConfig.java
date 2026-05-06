22598df8d5f48bbd98a89ed72da9778bc8043699
João Paulo <jpoltramari@gmail.com>
2026-05-06 13:09:39 -0300

    feat: implementa RBAC autorização usando permissoes e hasAuthority

--- src/main/java/com/jpoltramari/library_api/infrastructure/security/SecurityConfig.java
+++ src/main/java/com/jpoltramari/library_api/infrastructure/security/SecurityConfig.java
@@ -22,19 +22,44 @@ public class SecurityConfig {
     }
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http, ApiSecurityExceptionHandler apiSecurityExceptionHandler) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http,
+                                                   ApiSecurityExceptionHandler apiSecurityExceptionHandler) throws Exception {
 
         http
                 .cors(Customizer.withDefaults())
                 .csrf(AbstractHttpConfigurer::disable)
+
                 .exceptionHandling(ex -> ex
                         .authenticationEntryPoint(apiSecurityExceptionHandler)
-                        .accessDeniedHandler(apiSecurityExceptionHandler))
+                        .accessDeniedHandler(apiSecurityExceptionHandler)
+                )
+
                 .authorizeHttpRequests(auth -> auth
+
                         .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         .requestMatchers("/auth/**").permitAll()
                         .requestMatchers(HttpMethod.POST,"/users").permitAll()
                         .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
+
+                        .requestMatchers(HttpMethod.GET, "/books/**").authenticated()
+                        .requestMatchers(HttpMethod.POST, "/books/**").hasAuthority("CREATE_BOOK")
+                        .requestMatchers(HttpMethod.PUT, "/books/**").hasAuthority("CREATE_BOOK")
+                        .requestMatchers(HttpMethod.DELETE, "/books/**").hasAuthority("CREATE_BOOK")
+
+                        .requestMatchers(HttpMethod.GET, "/authors/**").authenticated()
+                        .requestMatchers(HttpMethod.POST, "/authors/**").hasAuthority("CREATE_AUTHOR")
+                        .requestMatchers(HttpMethod.PUT, "/authors/**").hasAuthority("CREATE_AUTHOR")
+                        .requestMatchers(HttpMethod.DELETE, "/authors/**").hasAuthority("CREATE_AUTHOR")
+
+                        .requestMatchers(HttpMethod.POST, "/loans").hasAuthority("REQUEST_LOAN")
+                        .requestMatchers(HttpMethod.GET, "/loans/**").hasAuthority("VIEW_ALL_LOANS")
+
+                        .requestMatchers(HttpMethod.PUT, "/loans/*/approve").hasAuthority("APPROVE_LOAN")
+                        .requestMatchers(HttpMethod.PUT, "/loans/*/withdraw").hasAuthority("REGISTER_WITHDRAWAL")
+                        .requestMatchers(HttpMethod.PUT, "/loans/*/return").hasAuthority("REGISTER_RETURN")
+
+                        .requestMatchers(HttpMethod.GET, "/users/**").hasAuthority("VIEW_ALL_USERS")
+
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class);
