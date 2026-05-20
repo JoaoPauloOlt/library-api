2f750b269b74eb6c5468d0bd51c11ebd7420a9ca
João Paulo <jpoltramari@gmail.com>
2026-05-20 17:46:00 -0300

    feature: adicionando generos como enum

---
+++ src/main/java/com/jpoltramari/library_api/domain/enums/Genre.java
@@ -0,0 +1,15 @@
+package com.jpoltramari.library_api.domain.enums;
+
+public enum Genre {
+    ACTION,
+    ADVENTURE,
+    CLASSIC,
+    COMIC,
+    DRAMA,
+    FANTASY,
+    HISTORICAL,
+    HORROR,
+    MYSTERY,
+    ROMANCE,
+    SCIENCE_FICTION,
+}
