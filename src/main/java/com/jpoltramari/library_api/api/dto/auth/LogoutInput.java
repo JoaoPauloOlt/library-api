3adf76881b039204b68dfda14daa6200ac3c095f
João Paulo <jpoltramari@gmail.com>
2026-05-25 11:54:31 -0300

    refactor: Adicinando notBlank para remover control no service

--- src/main/java/com/jpoltramari/library_api/api/dto/auth/LogoutInput.java
+++ src/main/java/com/jpoltramari/library_api/api/dto/auth/LogoutInput.java
@@ -1,6 +1,12 @@
 package com.jpoltramari.library_api.api.dto.auth;
 
+import jakarta.validation.constraints.NotBlank;
+
 public record LogoutInput(
+
+        @NotBlank
         String refreshToken,
+
+        @NotBlank
         String accessToken
 ) {}
