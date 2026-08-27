5c9d20622881724aaee238f33a9938399c2e1994
João Paulo Oltramari <jpoltramari@gmail.com>
2026-08-27 11:48:33 -0300

    feat(book): expose description in book model

--- src/main/java/com/jpoltramari/library_api/api/dto/book/BookModel.java
+++ src/main/java/com/jpoltramari/library_api/api/dto/book/BookModel.java
@@ -1,22 +1,19 @@
 package com.jpoltramari.library_api.api.dto.book;
 
 import com.jpoltramari.library_api.api.dto.author.AuthorModel;
-
 import java.time.LocalDateTime;
 import java.util.List;
 
 public record BookModel(
-
         Long id,
         String isbn,
         String title,
         String genre,
         LocalDateTime createdAt,
         String coverUrl,
-
+        String description,
         Long totalCopies,
         Long availableCopies,
         Long loanCount,
-
         List<AuthorModel> authors
 ) {}
