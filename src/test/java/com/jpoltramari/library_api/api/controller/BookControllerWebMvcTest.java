edd690693b5f73c3223f5849643ff9b3c328ca68
João Paulo Oltramari <jpoltramari@gmail.com>
2026-09-16 12:06:28 -0300

    test: use real JWT filter in book controller tests

--- src/test/java/com/jpoltramari/library_api/api/controller/BookControllerWebMvcTest.java
+++ src/test/java/com/jpoltramari/library_api/api/controller/BookControllerWebMvcTest.java
@@ -5,7 +5,6 @@ import com.jpoltramari.library_api.api.dto.book.BookModel;
 import com.jpoltramari.library_api.api.mapper.BookMapper;
 import com.jpoltramari.library_api.domain.model.Book;
 import com.jpoltramari.library_api.domain.service.BookService;
-import com.jpoltramari.library_api.infrastructure.security.JwtAuthenticationFilter;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -36,9 +35,6 @@ class BookControllerWebMvcTest {
 
     @MockBean
     private BookMapper mapper;
-
-    @MockBean
-    private JwtAuthenticationFilter jwtAuthenticationFilter;
 
     @Test
     void shouldRejectBookListWithoutAuthentication() throws Exception {
