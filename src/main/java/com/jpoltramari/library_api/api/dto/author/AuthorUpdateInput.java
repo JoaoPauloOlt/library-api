1d976f1fc8a9f16f2c77deffd9bd59d13405721d
João Paulo Oltramari <jpoltramari@gmail.com>
2026-09-17 12:54:07 -0300

    docs: describe author update schema

--- src/main/java/com/jpoltramari/library_api/api/dto/author/AuthorUpdateInput.java
+++ src/main/java/com/jpoltramari/library_api/api/dto/author/AuthorUpdateInput.java
@@ -1,12 +1,15 @@
 package com.jpoltramari.library_api.api.dto.author;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Size;
 
+@Schema(name = "AuthorUpdateInput", description = "Fields that can be changed when updating an author. All fields are optional.")
 public record AuthorUpdateInput(
-
+        @Schema(description = "Author name.", example = "George Orwell", maxLength = 100)
         @Size(max = 100)
         String name,
 
+        @Schema(description = "Author nationality.", example = "British", maxLength = 40)
         @Size(max = 40)
         String nationality
 ) {}
