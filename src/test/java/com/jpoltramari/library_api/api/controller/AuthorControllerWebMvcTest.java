2a595457ce30d8f79fad3095a297812283851dd6
João Paulo Oltramari <jpoltramari@gmail.com>
2026-09-17 14:31:37 -0300

    test: add AuthorController security coverage (#24)

---
+++ src/test/java/com/jpoltramari/library_api/api/controller/AuthorControllerWebMvcTest.java
@@ -0,0 +1,59 @@
+package com.jpoltramari.library_api.api.controller;
+
+import com.jpoltramari.library_api.LibraryApiApplication;
+import com.jpoltramari.library_api.api.mapper.AuthorMapper;
+import com.jpoltramari.library_api.domain.service.AuthorService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest(classes = LibraryApiApplication.class)
+@AutoConfigureMockMvc
+class AuthorControllerWebMvcTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private AuthorService service;
+
+    @MockBean
+    private AuthorMapper mapper;
+
+    @Test
+    void shouldRejectAuthorListWithoutAuthentication() throws Exception {
+        mockMvc.perform(get("/authors"))
+                .andExpect(status().isUnauthorized());
+    }
+
+    @Test
+    void shouldRejectAuthorListWithoutReadPermission() throws Exception {
+        mockMvc.perform(get("/authors")
+                        .with(user("user")))
+                .andExpect(status().isForbidden());
+    }
+
+    @Test
+    void shouldAllowAuthorListWithReadPermission() throws Exception {
+        mockMvc.perform(get("/authors")
+                        .with(user("librarian")
+                                .authorities(new SimpleGrantedAuthority("AUTHOR_READ"))))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void shouldRejectInvalidAuthorId() throws Exception {
+        mockMvc.perform(get("/authors/0")
+                        .with(user("librarian")
+                                .authorities(new SimpleGrantedAuthority("AUTHOR_READ"))))
+                .andExpect(status().isBadRequest());
+    }
+}
