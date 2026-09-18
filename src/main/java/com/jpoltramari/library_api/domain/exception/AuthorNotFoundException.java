fba19316e23a44eac0b775a89a2b21cf7ca909e8
João Paulo Oltramari <jpoltramari@gmail.com>
2026-09-18 16:01:34 -0300

    refactor(exception): simplify author not-found exception

--- src/main/java/com/jpoltramari/library_api/domain/exception/AuthorNotFoundException.java
+++ src/main/java/com/jpoltramari/library_api/domain/exception/AuthorNotFoundException.java
@@ -2,7 +2,7 @@ package com.jpoltramari.library_api.domain.exception;
 
 public class AuthorNotFoundException extends EntityNotFoundException {
 
-    public AuthorNotFoundException(Long id){
+    public AuthorNotFoundException(Long id) {
         super(String.format("Author with id %d was not found", id));
     }
 }
