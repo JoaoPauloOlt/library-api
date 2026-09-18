42ed545c39ced2a81eabbb5d3e8081d5d1e9772e
João Paulo Oltramari <jpoltramari@gmail.com>
2026-09-18 14:53:03 -0300

    fix: import JwtService in coverage test

--- src/test/java/com/jpoltramari/library_api/infrastructure/security/jwt/JwtInfrastructureCoverageTest.java
+++ src/test/java/com/jpoltramari/library_api/infrastructure/security/jwt/JwtInfrastructureCoverageTest.java
@@ -5,6 +5,7 @@ import com.jpoltramari.library_api.domain.model.User;
 import com.jpoltramari.library_api.domain.repository.RefreshTokenRepository;
 import com.jpoltramari.library_api.domain.repository.UserRepository;
 import com.jpoltramari.library_api.domain.repository.UserSecuritySnapshotView;
+import com.jpoltramari.library_api.infrastructure.security.JwtService;
 import com.jpoltramari.library_api.domain.service.TokenVersionService;
 import com.jpoltramari.library_api.infrastructure.security.rbac.RbacResolver;
 import com.jpoltramari.library_api.infrastructure.security.snapshot.CachedUserSecuritySnapshotAdapter;
