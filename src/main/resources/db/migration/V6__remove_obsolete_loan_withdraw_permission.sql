dd71eb5b23b3c88438536093cb5c1ca80376377c
João Paulo Oltramari <jpoltramari@gmail.com>
2026-09-18 12:59:49 -0300

    fix: remove duplicate Flyway version 6 migration

--- src/main/resources/db/migration/V6__remove_obsolete_loan_withdraw_permission.sql
+++
@@ -1,9 +0,0 @@
-DELETE FROM group_permissions
-WHERE permission_id = (
-    SELECT id
-    FROM permissions
-    WHERE name = 'LOAN_WITHDRAW'
-);
-
-DELETE FROM permissions
-WHERE name = 'LOAN_WITHDRAW';
