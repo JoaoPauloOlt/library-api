771c845baac277281cc5949273e94895a902fc61
João Paulo Oltramari <jpoltramari@gmail.com>
2026-09-23 14:39:31 -0300

    refactor: update test service imports

--- src/test/java/com/jpoltramari/library_api/api/controller/AuthorControllerWebMvcTest.java
+++ src/test/java/com/jpoltramari/library_api/api/controller/AuthorControllerWebMvcTest.java
@@ -2,7 +2,7 @@ package com.jpoltramari.library_api.api.controller;
 
 import com.jpoltramari.library_api.LibraryApiApplication;
 import com.jpoltramari.library_api.api.mapper.AuthorMapper;
-import com.jpoltramari.library_api.domain.service.AuthorService;
+import com.jpoltramari.library_api.application.service.AuthorService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
